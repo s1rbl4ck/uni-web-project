3807062eca36f5415f3a1e694f12febaa1965bcd
s1rbl4ck <danesh.hb82@gmail.com>
2022-05-13 13:56:40 +0430

    footer completed & authentication pages has been completed as well and a package added to the project like toastify

--- js/custom.js
+++ js/custom.js
@@ -69,3 +69,49 @@ $(".slider").slick({
     // instead of a settings object
   ],
 });
+
+// Handle Form
+$(".auth-form").submit(function (e) {
+  e.preventDefault();
+
+  var type = $(this).attr("type");
+  var message;
+  if (type === "login") {
+    message = "ورود با موفقیت انجام شد";
+  } else {
+    message = "حساب کاربری شما با موفقیت ساخته شد";
+  }
+  var button = $(this).find("button");
+  var text = button.text();
+  button.html("<div class='spinner-border spinner-border-sm text-light' role='status'><span class='visually-hidden'>Loading...</span></div>");
+  setTimeout(() => {
+    button.html(text);
+    Toastify({
+      text: message,
+      duration: 3000,
+      close: false,
+      gravity: "bottom", // `top` or `bottom`
+      position: "right", // `left`, `center` or `right`
+      rtl: true,
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      backgroundColor: "#2dcd95",
+    }).showToast();
+    setTimeout(() => {
+      window.location.href = "index.html";
+    }, 1500);
+  }, 2000);
+});
+
+// Password Input Visible Status
+$(".visible-status").click(function () {
+  var input = $(this).prev();
+  if (this.classList.contains("active")) {
+    this.classList.remove("active");
+    input.attr("type", "password");
+    $(this).html("<ion-icon name='eye-outline'></ion-icon>");
+  } else {
+    this.classList.add("active");
+    input.attr("type", "text");
+    $(this).html("<ion-icon name='eye-off-outline'></ion-icon>");
+  }
+});
